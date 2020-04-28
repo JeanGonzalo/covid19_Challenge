4d9eb6969d89691c50e58031880c36190c74adaa
jean <rjeangonzalo@gmail.com>
2020-04-28 10:08:55 -0500

    [fix] models

---
+++ lib/models/global.dart
@@ -0,0 +1,17 @@
+class Global {
+  int newConfirmed;
+  int totalConfirmed;
+  int newDeaths;
+  int totalDeaths;
+  int newRecovered;
+  int totalRecovered;
+
+  Global({
+    this.newConfirmed,
+    this.totalConfirmed,
+    this.newDeaths,
+    this.totalDeaths,
+    this.newRecovered,
+    this.totalRecovered,
+  });
+}
